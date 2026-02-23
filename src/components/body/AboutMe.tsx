76896b9d54b1c99a43794289d3cb63533698ca36
Precious <preciousclaudia555@gmail.com>
2026-02-23 17:12:18 +0800

    fix: about me

--- src/components/body/AboutMe.tsx
+++ src/components/body/AboutMe.tsx
@@ -15,16 +15,21 @@ export default function AboutMe() {
         <h1 className="text-2xl font-bold">About</h1>
       </div>
       <div className="flex-wrap">
-        I am a passionate web developer with experience in building responsive and user-friendly web applications.I am
-        proficient in HTML, CSS, JavaScript, and React, and I am always eager to learn new technologies and improve my
-        skills. I am a team player and I enjoy collaborating with others to create innovative solutions. I am excited to
-        contribute my skills and knowledge to a dynamic and forward-thinking organization.
+        I am a passionate web developer with experience in building responsive
+        and user-friendly web applications. I am proficient in HTML, CSS,
+        JavaScript, and React, and I am always eager to learn new technologies
+        and improve my skills. I am a team player and I enjoy collaborating with
+        others to create innovative solutions. I am excited to contribute my
+        skills and knowledge to a dynamic and forward-thinking organization.
       </div>
       <div>
-        I also do Wordpress Web Development and also doing some Video editing for fun because that is also a creative
-        outlet for me. I have experience in using video editing software such as Adobe Premiere Pro and I enjoy creating
-        engaging and visually appealing videos. Whether it&apos;s for personal projects or professional work, I am
-        always looking for ways to improve my video editing skills and create content that resonates with my audience.
+        I also do Wordpress Web Development and also doing some Video editing
+        for fun because that is also a creative outlet for me. I have experience
+        in using video editing software such as Adobe Premiere Pro and I enjoy
+        creating engaging and visually appealing videos. Whether it&apos;s for
+        personal projects or professional work, I am always looking for ways to
+        improve my video editing skills and create content that resonates with
+        my audience.
       </div>
     </div>
   );
