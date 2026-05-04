681cb5f96f8479f71e90c5cb63082ead2e83433c
Ezekiel <ezekiel.salazar18@gmail.com>
2026-05-04 18:25:45 +0800

    fix: resume to cv

--- src/components/hero/Navigation.tsx
+++ src/components/hero/Navigation.tsx
@@ -13,7 +13,11 @@ import { BlurFade } from "@/components/ui/blur-fade";
 const Navigation = () => {
   return (
     <>
-      <BlurFade delay={0.25} blur="10px" inView={true}>
+      <BlurFade
+        delay={0.25}
+        blur="10px"
+        inView={true}
+      >
         <div className="fixed top-4 right-4 z-50 md:hidden">
           <ThemeButton />
         </div>
@@ -30,9 +34,7 @@ const Navigation = () => {
           <div className="flex flex-col gap-2 justify-center w-full items-center md:items-start text-center md:text-left">
             <div className="flex items-center w-full justify-center md:justify-between">
               <div className="flex items-center gap-2">
-                <h1 className="font-bold text-2xl md:text-4xl text-accent-foreground">
-                  Ezekiel Salazar
-                </h1>
+                <h1 className="font-bold text-2xl md:text-4xl text-accent-foreground">Ezekiel Salazar</h1>
                 <Image
                   src={logo.src}
                   alt="logo"
@@ -56,12 +58,16 @@ const Navigation = () => {
               />
               Mandaluyong City, Philippines
             </h1>
-            <h1 className="text-sm md:text-base">
-              BS Information Technology / Web Developer
-            </h1>
+            <h1 className="text-sm md:text-base">BS Information Technology / Web Developer</h1>
             <div className="flex flex-row sm:flex-row gap-3 md:gap-4 w-full justify-center md:justify-start lg:justify-start">
-              <Button className="cursor-pointer" asChild>
-                <a download="Salazar, Ezekiel_RESUME.pdf" href="/RESUME.pdf">
+              <Button
+                className="cursor-pointer"
+                asChild
+              >
+                <a
+                  download="Salazar, Ezekiel_RESUME.pdf"
+                  href="/RESUME.pdf"
+                >
                   <Image
                     src={file.src}
                     alt="file"
@@ -69,11 +75,15 @@ const Navigation = () => {
                     height={20}
                     className="color-muted-foreground dark:invert"
                   />
-                  Download Resume
+                  Download CV
                 </a>
               </Button>
 
-              <Button variant="secondary" className="cursor-pointer" asChild>
+              <Button
+                variant="secondary"
+                className="cursor-pointer"
+                asChild
+              >
                 <a
                   href="https://mail.google.com/mail/?view=cm&to=ezekiel.salazar18@gmail.com&"
                   target="_blank"
